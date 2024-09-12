27d02f26cda9b46429913534ac306f49093b1212
Trung <22028170@vnu.edu.vn>
2024-09-12 08:06:32 +0700

    6

---
+++ Unit1/app/src/main/java/com/example/myapplication/Ec_Ghepchuoi.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+fun main() {
+    val numberOfAdults : Int = 20
+    val numberOfKids : Int = 30
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+}
---
+++ Unit1/app/src/main/java/com/example/myapplication/Ec_MauChuoi.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+
+fun main() {
+    var discountPercentage: Int = 0
+    var offer: String = ""
+    var item = "Google Chromecast"
+    discountPercentage = 20
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+
+    println(offer)
+}
